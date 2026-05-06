146b37b8ab6c0fbcadfaa8b7374698e0abbda30e
Nicoleliang123 <nicole.janet123@gmail.com>
2026-05-06 17:46:58 -0400

    Update UrlRepository.java

--- urlshortener/src/main/java/com/nicole/urlshortener/UrlRepository.java
+++ urlshortener/src/main/java/com/nicole/urlshortener/UrlRepository.java
@@ -2,7 +2,7 @@ package com.nicole.urlshortener;
 
 import org.springframework.data.repository.CrudRepository;
 
-//Manage UrlEntry table & @Id is String
-public interface UrlRepository extends CrudRepository<UrlEntry, String> {
+//Manage UrlEntity table & @Id is String
+public interface UrlRepository extends CrudRepository<UrlEntity, String> {
 
 }
